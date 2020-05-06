b07647680f73dd4bd8851fb38364c16e0974b78d
pratikraut4 <pratikraut41200.pr@gmail.com>
2020-05-06 18:34:18 +0530

    first commit

--- index.php
+++ index.php
@@ -1,9 +1,104 @@
 <html>
     <head>
-        <title>Index of Website</title> 
-        
+        <title>Index of Website</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     </head>
 <body>
-    <h1>Hello world</h1>
+
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse  justify-content-center" id="navbarSupportedContent">
+          <ul class="navbar-nav ">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About US</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact US</a>
+            </li>
+          </ul>
+        </div>
+        <ul class="navbar-nav navbar-right">
+          <li class="nav-item"><a class="nav-link" href = "#"><i class="fa fa-user-circle" aria-hidden="true"></i>SignUP</a></li>
+          <li class="nav-item"><a class="nav-link" href = "#"><i class="fa fa-user-o" aria-hidden="true"></i>LOGIN</a></li>
+
+        </ul>
+      </div>
+    </nav>
+
+
+
+        <div class="footer">
+          <div class="footer-content">
+            <div class="footer-section about">
+              <h1 class="foot-text"><span>Our</span>Company</h1>
+              <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout</p>
+
+            </div>
+            <div class="footer-section links">
+              <h2>Quick Links</h2><br>
+              <ul>
+                <a href="#"><li>EVENTS</li></a>
+                <a href="#"><li>TEAN</li></a>
+                <a href="#"><li>MENTORS</li></a>
+                <a href="#"><li>GALLERY</li></a>
+                <a href="#"><li>TERMS & CONDITION</li></a>
+              </ul>
+            </div>
+            <div class="footer-section contact-form">
+              <h2>Contact US</h2>
+              <br>
+              <div class="contact">
+                <span><i class="fa fa-phone" aria-hidden="true">&nbsp 123-456-7890</i></span>
+                <span><i class="fa fa-envelope" aria-hidden="true">&nbsp https@gmailcom</i></span>
+
+              </div>
+              <div class="social">
+                <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+
+              </div>
+            </div>
+
+          </div>
+          <div class="footer-bottom">
+            &copy; 2020 | BY- itsrai, z0re, harRy, r@ut
+
+          </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
