8c78f2035a5a46f3a6e12deecca65f7ae27be86f
Sakharam <sakharamzore31@gmail.com>
2020-05-24 02:32:45 +0530

    header, footer and about php files are added.

---
+++ header/header.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="header.css">
+</head>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon "></span>
+    </button>
+
+    <div class="collapse navbar-collapse  justify-content-center " id="navbarSupportedContent">
+      <ul class="navbar-nav text-center ml-auto ">
+        <li class="nav-item active  mx-4">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item  mx-4">
+          <a class="nav-link" href="about.php">About US</a>
+        </li>
+        <li class="nav-item  mx-4">
+          <a class="nav-link" href="#">Services</a>
+        </li>
+        <li class="nav-item  mx-4">
+          <a class="nav-link" href="#">Contact US</a>
+        </li>
+
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
+</html>
--- index.php
+++ index.php
@@ -14,35 +14,10 @@
 
     </head>
 <body>
-
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">LOGO</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon "></span>
-      </button>
-
-      <div class="collapse navbar-collapse  justify-content-center " id="navbarSupportedContent">
-        <ul class="navbar-nav text-center ml-auto ">
-          <li class="nav-item active  mx-4">
-            <a class="nav-link" href="#">Home</a>
-          </li>
-          <li class="nav-item  mx-4">
-            <a class="nav-link" href="about.php">About US</a>
-          </li>
-          <li class="nav-item  mx-4">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item  mx-4">
-            <a class="nav-link" href="#">Contact US</a>
-          </li>
-
-
-        </ul>
-      </div>
-
-    </div>
-  </nav>
+<?php
+// header added
+include 'header/header.php';
+?>
 <div class="container-fluid slide">
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
@@ -91,141 +66,11 @@
     </a>
   </div>
 </div>
-
 </div>
-
-
-  <div class="footer">
-    <footer class="bg-dark py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <div class="row">
-              <div class="accordion w-100 d-lg-flex" id="accordion">
-                <div class="col-lg-4 col-md-12">
-                  <a href="#one" data-toggle="collapse"
-                    class="h5 text-uppercase text-white nav-link p-0 d-block d-lg-none d-xl-none">Useful Links</a>
-
-                  <div class="h5 text-uppercase text-white p-0 d-none d-lg-block d-xl-block">
-                    Our Company
-                  </div>
-
-                  <div class="collapse text-white d-lg-flex"
-                    id="one"
-                    data-parent="#accordion">
-                    <ul class="list-unstyled">
-                      <li>
-                        <a href="#" class="nav-link p-0 text-white">Our Team</a>
-                      </li>
-                      <li>
-                        <a href="#" class="nav-link p-0 text-white">Contact</a>
-                      </li>
-                      <li>
-                        <a href="#" class="nav-link p-0 text-white">About</a>
-                      </li>
-                      <li>
-                        <a href="#" class="nav-link p-0 text-white">Services</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-12">
-                  <a
-                    href="#two"
-                    data-toggle="collapse"
-                    class="h5 text-uppercase text-white nav-link p-0 d-block d-lg-none d-xl-none"
-                    >Two</a
-                  >
-                  <div
-                    class="h5 text-uppercase text-white p-0 d-none d-lg-block d-xl-block"
-                  >
-                    Description
-                  </div>
-                  <div
-                    class="collapse text-white d-lg-flex"
-                    id="two"
-                    data-parent="#accordion">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Itaque ratione quis doloribus, numquam ipsam voluptatibus
-                    quia harum molestiae officia dolorum deserunt nihil soluta
-                    dignissimos quibusdam cum nulla, similique illum. Rerum.
-                  </div>
-                </div>
-                <div class="col-lg-4 col-md-12">
-                  <a
-                    href="#three"
-                    data-toggle="collapse"
-                    class="h5 text-uppercase text-white nav-link p-0 d-block d-lg-none d-xl-none"
-                    >Three</a
-                  >
-                  <div
-                    class="h5 text-uppercase text-white p-0 d-none d-lg-block d-xl-block"
-                  >
-                    SOCIAL MEDIA
-                  </div>
-                  <div
-                    class="collapse text-white d-lg-flex"
-                    id="three"
-                    data-parent="#accordion"
-                  >
-                    <div class="d-flex w-100 justify-content-between">
-                      <a href="#" class="icon"
-                        ><i class="fab fa-facebook"></i
-                      ></a>
-                      <a href="#" class="icon"
-                        ><i class="fab fa-twitter"></i
-                      ></a>
-                      <a href="#" class="icon"
-                        ><i class="fab fa-instagram"></i
-                      ></a>
-                      <a href="#" class="icon"
-                        ><i class="fab fa-youtube"></i
-                      ></a>
-                      <a href="#" class="icon"
-                        ><i class="fab fa-linkedin"></i
-                      ></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <div class="container-fluid">
-      <div class="row bg-danger">
-        <div class="col-md-12">
-          <div class="d-flex justify-content-center align-items-center">
-            <span class="p-3 text-white">WEBPROJECT &copy; Copyright 2019</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php
+// footer added
+include 'footer/footer.php';
+?>
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
